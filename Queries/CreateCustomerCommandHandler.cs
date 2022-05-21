0400d5539a1aaad188c69cddd0ac064d2b7c0368
imankns70 <45065474+imankns70@users.noreply.github.com>
2022-05-21 17:57:31 +0330

    .

--- Queries/CreateCustomerCommandHandler.cs
+++ Queries/CreateCustomerCommandHandler.cs
@@ -1,6 +1,18 @@
-﻿namespace CQRS.Queries
+﻿using AutoMapper;
+using CQRS.Data;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS.Queries
 {
-    public class CreateCustomerCommandHandler
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, CustomerDto>
     {
+        readonly ApplicationDbContext _context;
+        readonly IMapper _mapper;
+        public Task<CustomerDto> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
